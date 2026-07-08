1fe535f859c0dc6d95b7ecc8c87663301b0fb0a6
Fey Xie <feyxiexzf@gmail.com>
2026-07-08 08:57:16 +0800

    remove testing code

--- app/src/main/java/nuke/data/cipher/NativeCrypto.kt
+++ app/src/main/java/nuke/data/cipher/NativeCrypto.kt
@@ -1,39 +1,39 @@
-package nuke.data.cipher
-
-object NativeCrypto {
-    @JvmStatic
-    external fun nativeAttestModule(bArr: ByteArray): Boolean
-
-    @JvmStatic
-    external fun nativeConfDecrypt(str: String): String
-
-    @JvmStatic
-    external fun nativeConfEncrypt(str: String): String
-
-    @JvmStatic
-    external fun nativeConfKeyName(str: String): String
-
-    @JvmStatic
-    external fun nativeDecryptJsonBytes(
-        str: String,
-        str2: String,
-        str3: String,
-        str4: String,
-        str5: String
-    ): ByteArray
-
-    @JvmStatic
-    external fun nativeEncryptJsonBytes(bArr: ByteArray?, str: String?): Array<String>
-
-    @JvmStatic
-    external fun nativeIsConfigured(): Boolean
-
-    @JvmStatic
-    external fun nativeKid(): String
-
-    @JvmStatic
-    external fun nativeSelfTest(): Boolean
-
-    @JvmStatic
-    external fun nativeSignClientPayload(str: String): String
-}
+//package nuke.data.cipher
+//
+//object NativeCrypto {
+//    @JvmStatic
+//    external fun nativeAttestModule(bArr: ByteArray): Boolean
+//
+//    @JvmStatic
+//    external fun nativeConfDecrypt(str: String): String
+//
+//    @JvmStatic
+//    external fun nativeConfEncrypt(str: String): String
+//
+//    @JvmStatic
+//    external fun nativeConfKeyName(str: String): String
+//
+//    @JvmStatic
+//    external fun nativeDecryptJsonBytes(
+//        str: String,
+//        str2: String,
+//        str3: String,
+//        str4: String,
+//        str5: String
+//    ): ByteArray
+//
+//    @JvmStatic
+//    external fun nativeEncryptJsonBytes(bArr: ByteArray?, str: String?): Array<String>
+//
+//    @JvmStatic
+//    external fun nativeIsConfigured(): Boolean
+//
+//    @JvmStatic
+//    external fun nativeKid(): String
+//
+//    @JvmStatic
+//    external fun nativeSelfTest(): Boolean
+//
+//    @JvmStatic
+//    external fun nativeSignClientPayload(str: String): String
+//}
